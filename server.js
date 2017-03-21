0848ebcd43e7f3581e945d9b1b17354579ea5285
cfv7 <hq@colin.red>
2017-03-21 16:34:17 -0400

    Updated Dependencies

--- server.js
+++ server.js
@@ -2,4 +2,18 @@
 // assignment: Blog API II
 // date: 3.21.17
 /*----------------------------*/
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {BLOGPOSTS} = require('./models');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/blogposts', (req, res) => {
+
+})
