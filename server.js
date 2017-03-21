e7b6771bedc2ae0b6b9e222d3a6c55da9c7c9d12
cfv7 <hq@colin.red>
2017-03-21 16:56:54 -0400

    working server

--- server.js
+++ server.js
@@ -14,6 +14,33 @@ const {BLOGPOSTS} = require('./models');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/blogposts', (req, res) => {
-
+app.get('/blog-post', (req, res) => {
+  res.send('hello')
 })
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// `closeServer` function is here in original code
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
